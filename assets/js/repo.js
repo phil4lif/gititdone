01a384a1ab5fac16de94d49fba43bb51c9c2bb01
phil cowan <philipjcowan1@gmail.com>
2021-03-03 13:22:41 -0700

    add link to repo for issues beyond 30

--- assets/js/repo.js
+++ assets/js/repo.js
@@ -1,4 +1,15 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
+
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = 'Showing first 30 issues ';
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'See More Issues on GitHub.com';
+    linkEl.setAttribute('href', `https://github.com/${repo}/issues`);
+    linkEl.setAttribute('target', '_blank');
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getIssues = function(repo) {
     var apiURL = `https://api.github.com/repos/${repo}/issues`;
@@ -6,14 +17,20 @@ var getIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data){
                 displayIssues(data);
+                if (response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
             })
         } else {
             alert('Something went wrong with this request');
         }
     });
-}
+};
 
 var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = 'This Repo Has No Open Issues or PR\'s'
+    }
     for (var i = 0; i < issues.length; i++) {
         var issueEl = document.createElement('a');
         issueEl.classList = 'list-item flex-row justify-space-between align-center';
@@ -34,6 +51,6 @@ var displayIssues = function(issues) {
         }
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
-    }
-}
-getIssues('phil4lif/gititdone')
+    };
+};
+getIssues('facebook/react')
